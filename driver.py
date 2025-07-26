14145ffe95ffb27a8d93028b224cf79c87f2429e
Przemek <przemyslaw.makowka@o2.pl>
2025-07-26 21:37:52 +0200

    Added loging utils

--- driver.py
+++ driver.py
@@ -8,14 +8,16 @@ class Driver:
 
     def initial_driver(self, browser_type: BrowserTypes) -> webdriver:
         if not self.__driver:
-            self.__log.info("Starting initializing browser...")
+            self.__log.info("Starting initializing browser")
             match browser_type:
                 case BrowserTypes.CHROME:
+                    self.__log.info("Setup Chrome browser")
                     chrome_options = webdriver.ChromeOptions()
                     chrome_options.add_argument("--start-maximized")
                     self.__driver = webdriver.Chrome(options=chrome_options)
                     self.__log.info("Chrome browser was initialized")
                 case BrowserTypes.FIREFOX:
+                    self.__log.info("Setup Firefox browser")
                     self.__driver = webdriver.Firefox()
                     self.__driver.maximize_window()
                     self.__log.info("Firefox browser was initialized")
@@ -31,9 +33,11 @@ class Driver:
 
     def get(self, url):
         if url:
+            self.__log.info(f"Navigate to {url}")
             self.__driver.get(url)
 
     def quit(self):
         if self.__driver:
+            self.__log.info("driver quit()")
             self.__driver.quit()
             self.__driver = None
